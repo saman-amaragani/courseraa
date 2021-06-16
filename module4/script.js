1fad7799650e6d90263cce4c1073ff866968ab80
saman amargani <samanamargani7@gmail.com>
2021-06-16 21:59:55 +0530

    initial

--- module4/script.js
+++ module4/script.js
@@ -1,11 +1,38 @@
-(function() {
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+(function () {
+
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
+  
     if (firstLetter === 'j') {
-      //byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
+      byeSpeaker(names[i]);
+    }
+    else {
+      helloSpeaker(names[i]);
     }
   }
-})();
+  
+  })();
